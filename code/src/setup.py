20282dbc61f29e30036ba0fdc6b800279a296fed
Euan Turner <euanoturner@gmail.com>
2021-11-01 11:54:55 +0000

    Initial setup of database

---
+++ code/src/setup.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect("Connect4.db")
+cursor = connection.cursor()
+
+##Define the table store within Connect4.db
+create_store = """
+CREATE TABLE store
+(
+fileID INTEGER,
+filetype INTEGER,
+data TEXT,
+primary key (fileID)
+)
+"""
+
+cursor.execute(create_store)
+connection.commit()
+connection.close()
