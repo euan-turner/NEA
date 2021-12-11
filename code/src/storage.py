66b9d4de6495d5bc93c819b5e0f7eceddc983814
Euan Turner <euanoturner@gmail.com>
2021-12-11 15:22:34 +0000

    Added select file functionality to storage

--- code/src/storage.py
+++ code/src/storage.py
@@ -1,6 +1,7 @@
 import sqlite3
 from auxiliary import Save_Type
 from board import Board
+from prettytable import PrettyTable
 
 def save(file_type : Save_Type, board : Board, database : str):
     """Saves a position or game to the database
@@ -18,7 +19,7 @@ def save(file_type : Save_Type, board : Board, database : str):
     """
     execute_sql(sql_command, database)
 
-def load(file_type : Save_Type, database : str):
+def load(file_type : Save_Type, database : str) -> list:
     """Loads a position or game from the database
     Functionality will be altered when the account system is added
 
@@ -46,12 +47,12 @@ def convert(moves: list) -> str:
     """
     return ''.join([str(i) for i in moves])
 
-def execute_sql(command, database):
+def execute_sql(command : str, database : str) -> sqlite3.Cursor:
     """Executes an sql command using sqlite3
 
     Args:
         command (string): sqlite3 command
-        database (string): atabase to use
+        database (string): database to use
 
     Returns:
         sqlite3.Cursor : Results of sql command
@@ -61,4 +62,43 @@ def execute_sql(command, database):
     output = cursor.execute(command)
     conn.commit()
     return output
+
+def select_file(files : list):
+    table = PrettyTable()
+    table.field_names = ["#", "File"]
+    table.add_column("#", [i+1 for i in range(len(files))])
+    table.add_column("File", files)
+    print(table)
+
+    confirmed = False
+    while not confirmed: ##Validate and accept input
+        try:
+            choice = int(input("Enter file number: "))
+        except:
+            print("Enter an integer")
+            continue
+
+        if choice <= 0:
+            print("File index not found, try again")
+            continue
+
+        try:
+            selection = files[choice-1]
+        except:
+            print("File index not found, try again")
+            continue
+
+
+        while True: ##Validate and accept confirmation
+            print(f"Confirm selection: {selection}")
+            confirmation = input("(Y/N): ")
+            if confirmation.upper() not in {'Y', 'N'}:
+                print("Invalid entry")
+                continue
+            elif confirmation.upper() == 'Y':
+                confirmed = True
+            break
+
+    return selection
+
 
