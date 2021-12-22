c3194e1a7942d37c9c15b11fbd206f4727627a97
Euan Turner <euanoturner@gmail.com>
2021-12-22 20:52:27 +0000

    Initial main window design - without buttons

--- code/src/interface.py
+++ code/src/interface.py
@@ -1,8 +1,17 @@
 from prettytable import PrettyTable
 from board import Board
 import pygame
+import os
+
+pygame.init()
+
+##Colours
+BLACK = (0,0,0)
 
 class Interface:
+
+    def __init__(self, back_theme):
+        self.back_theme = back_theme
 
     def output_board(board : Board):
 
@@ -14,10 +23,70 @@ class Interface:
             table.add_row(row)
         print(table)
 
+    def create_window(self):
+        """Creates the main window used by the application
+        """
+        self.window = pygame.display.set_mode((600,400))
+        pygame.display.set_caption("Connect 4 Application")
+        self.window.fill(self.back_theme)
+        pygame.display.flip()
+
+    def home_window(self):
+        ##Path to hamburger menu image
+        menu_image_path = self.get_image_path('menu.png')
+        ##Path to board image
+        board_image_path = self.get_image_path('board.png')
+
+        ##Surface for menu
+        menu_surface = pygame.image.load(menu_image_path)
+        menu_surface = pygame.transform.scale(menu_surface, (50, 50))
+
+        ##Surface for board
+        board_surface = pygame.image.load(board_image_path)
+        board_surface = pygame.transform.scale(board_surface, (250,200))
+
+        ##Surface for text
+        font = pygame.font.SysFont('rockwell', 50)
+        text_surface = font.render('Connect 4', False, BLACK)
+
+
+
+        ##Centre board surface
+        board_x = (self.window.get_width() / 2) - (board_surface.get_width() / 2)
+        board_y = (self.window.get_height() / 2) - (board_surface.get_height() / 2)
+
+        ##Centre text surface
+        text_x = (self.window.get_width() / 2) - (text_surface.get_width() / 2)
+        text_y = (self.window.get_height() / 4) - text_surface.get_height()
+
+        self.window.blit(menu_surface, (5,5))
+        self.window.blit(board_surface, (board_x,board_y))
+        self.window.blit(text_surface, (text_x, text_y))
+
+        pygame.display.flip()
+
+    def get_image_path(self, filename : str) -> str:
+        """Gets the path of an image in the images directory
+        Assumes the following directory structure
+        src - interface.py
+        |
+        images
+
+        Args:
+            filename (str): Filename of desired image
+
+        Returns:
+            str: Path to desired image
+        """
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        image_path = os.path.join(file_path, '..', '..', 'images', filename)
+        return os.path.normpath(image_path)
+
+
 
 
 class Button():
-    """Generic button class
+    """Redefine a generic button class, and inheriting block, text and image button classes
     """
 
     def __init__(self, rect : pygame.Rect, colour : tuple,
