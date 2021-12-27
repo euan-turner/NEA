2b53cfb613c76ef8c45fab259fee40e4c348789e
Euan Turner <euanoturner@gmail.com>
2021-12-27 16:21:01 +0000

    Initial setup for game window

--- code/src/interface.py
+++ code/src/interface.py
@@ -1,7 +1,5 @@
 from prettytable import PrettyTable
 from board import Board
-from auxiliary import Main_Menu_Choice
-from time import sleep
 import pygame
 import os
 
@@ -11,6 +9,9 @@ pygame.init()
 ##Colours
 BLACK = (0,0,0)
 DEEP_RED = (129,27,27)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+WHITE = (255,255,255)
 class Button():
     """Redefine a generic button class, and inheriting block, text and image button classes
     """
@@ -109,8 +110,8 @@ class Interface:
 
         Returns:
             int: Code representing application component choice
-        """        
-
+        """
+        self.create_window()
         self.home_menu_window_setup()
 
         ##Main loop for home window
@@ -124,11 +125,11 @@ class Interface:
                     for button in self.active_buttons:
                         choice = button.check_click(event, self.window, self.base_theme)
                         ##Menu icon clicked - run menu options
-                        if choice == Main_Menu_Choice.menu:
+                        if choice == 'menu':
                             ##Clear menu button
                             button.clear(self.window, self.base_theme)
                             self.active_buttons.remove(button)
-                            self.home_menu_window_options()
+                            self.menu_options()
                             break ##Clear current event queue being processed
 
                         elif choice != None:
@@ -138,8 +139,7 @@ class Interface:
     def home_menu_window_setup(self):
         """Sets up the initial view and buttons for the home window
         """
-        ##Path to hamburger menu image
-        menu_image_path = self.get_image_path('menu.png')
+        self.menu_button_setup()
         ##Path to board image
         board_image_path = self.get_image_path('board.png')
 
@@ -159,31 +159,35 @@ class Interface:
         text_x = (self.window.get_width() / 2) - (text_surface.get_width() / 2)
         text_y = (self.window.get_height() / 4) - text_surface.get_height()
 
-        ##Surfaces for menu button
-        menu_surface = pygame.image.load(menu_image_path)
-        default_menu_surface = pygame.transform.scale(menu_surface, (50, 50))
-        hovered_menu_surface = pygame.transform.scale(menu_surface, (55, 55))
-        ##Clicked menu surface can just be default, as it will only be visible for an instant
-
-        menu_button = Button(pygame.Rect(5, 5, 60, 60), default_menu_surface,
-            hovered_menu_surface, default_menu_surface, Main_Menu_Choice.menu)
-        menu_button.update(self.window, self.base_theme)
-        self.active_buttons.append(menu_button)
-        ##self.window.blit(menu_surface, (5,5))
         self.window.blit(board_surface, (board_x,board_y))
         self.window.blit(text_surface, (text_x, text_y))
 
         pygame.display.flip()
 
-    def home_menu_window_options(self):
+    def menu_button_setup(self):
+        ##Path to hamburger menu image
+        menu_image_path = self.get_image_path('menu.png')
+
+        ##Surfaces for menu button
+        menu_surface = pygame.image.load(menu_image_path)
+        default_menu_surface = pygame.transform.scale(menu_surface, (50, 50))
+        hovered_menu_surface = pygame.transform.scale(menu_surface, (55, 55))
+        ##Clicked menu surface can just be default, as it will only be visible for an instant
+        menu_button = Button(pygame.Rect(5, 5, 60, 60), default_menu_surface,
+            hovered_menu_surface, default_menu_surface, 'menu')
+        menu_button.update(self.window, self.base_theme)
+        self.active_buttons.append(menu_button)
+
+    def menu_options(self):
         ##Text button labels
         labels = ["Play a Human", "Play the AI", "Load a Position", "Load a Game", "Quit the App"]
-        codes = [Main_Menu_Choice.play, Main_Menu_Choice.ai, Main_Menu_Choice.position, Main_Menu_Choice.game, Main_Menu_Choice.out]
+        codes = ['play', 'ai', 'position', 'game', 'out']
+
         for i in range(len(labels)):
             label = labels[i]
             code = codes[i]
             ##Surfaces
-            font = pygame.font.SysFont('rockwell', 20)
+            font = pygame.font.SysFont('rockwell', 10)
             default_surface = font.render(label, False, BLACK)
             hovered_surface = font.render(label, False, DEEP_RED)
             ##Position
@@ -194,6 +198,113 @@ class Interface:
             button = Button(rect, default_surface, hovered_surface, default_surface, code)
             button.update(self.window, self.base_theme)
             self.active_buttons.append(button)
+
+    def game_window(self, board : Board):
+        self.create_window()
+        self.menu_button_setup()
+        game_surface = self.game_window_setup()
+
+        ##Main loop for game window
+        cont = True
+        while cont:
+            ##Update all active buttons
+            for button in self.active_buttons:
+                button.update(self.window, self.base_theme)
+            ##Check for mouse clicks
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for button in self.active_buttons:
+                        choice = button.check_click(event, self.window, self.base_theme)
+                        print(choice)
+                        ##Menu clicked - run menu options
+                        if choice == 'menu':
+                            ##Clear menu button
+                            button.clear(self.window, self.base_theme)
+                            self.active_buttons.remove(button)
+                            self.menu_options()
+                            break
+                        ##Turn played
+                        elif choice in range(7):
+                            self.play_move(choice, board)
+                        ##Menu option selected
+                        elif choice != None:
+                            return choice
+
+    def game_window_setup(self) -> pygame.Surface:
+        ##Need to change to individual board surface
+        ##And separate method to create buttons for valid moves
+        ##Need to clear them for ai move
+        game_surface = self.game_surface_setup()
+        self.window.blit(game_surface, (125,50))
+        ##Add save button here
+        ##Add user icon here
+        ##Add turn buttons
+        for col in range(7):
+            label = str(col+1)
+            ##Surfaces
+            font = pygame.font.SysFont('rockwell', 20)
+            default_surface = font.render(label, False, BLACK)
+            hovered_surface = font.render(label, False, DEEP_RED)
+            ##Position
+            x = (150 + col*50) - (default_surface.get_width() / 2)
+            y = 360
+            rect = pygame.Rect(x,y, default_surface.get_width(), default_surface.get_height())
+            ##Create button
+            button = Button(rect, default_surface, hovered_surface, default_surface, col)
+            button.update(self.window, self.base_theme)
+            self.active_buttons.append(button)
+        return game_surface
+
+    def game_surface_setup(self) -> pygame.Surface:
+        game_surface = pygame.Surface((350,300))
+        game_surface.fill(self.base_theme)
+        ##Draw board
+        for row in range(6):
+            for col in range(7):
+                ##Co-ordindates are the centre of the back circle
+                x = 25 + col*50
+                y = 25 + row*50
+                pygame.draw.circle(game_surface, WHITE, (x,y), 22, width = 0)
+
+        return game_surface
+
+    def play_move(self, col : int, board : Board):
+        """Plays a move in the board and then game surface
+        Game window will need to be updated afterwards
+
+        Args:
+            col (int): Column to play in
+            board (Board): Current board instance
+        """
+        ##Play move in board
+
+        ##Identify location from col and heights
+        height = board.get_height(col) % 7
+        ##Work out (top, left) of token to be played
+
+        ##Blit token to game surface
+
+
+    def get_token(self, board : Board) -> pygame.Surface:
+        """Creates the token to be used to play a move
+
+        Args:
+            board (Board): Current board instance
+
+        Returns:
+            pygame.Surface: Token to be played
+        """
+        ##Identify current player using board counter
+        if board.get_counter() % 2 == 0:
+            colour = RED
+        else:
+            colour = YELLOW
+        ##Create surface
+        token = pygame.Surface((30,30))
+        token.fill(self.base_theme)
+        ##Colour circle depending on player
+        pygame.draw.circle(token, colour, (15, 15), 10, width = 0)
+        return token
 
 
 
